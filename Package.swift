98167fcd6db08b91fe910bbef61d1dc1d05c2849
Jorge Villalobos Beato <tiusender@gmail.com>
2022-05-07 21:55:12 -0400

    Adding more versions

--- Package.swift
+++ Package.swift
@@ -6,8 +6,10 @@ import PackageDescription
 let package = Package(
     name: "StretchingHeader",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v10_10),
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
